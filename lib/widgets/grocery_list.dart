1dce13e506cb47adb82b73514b7af4e9a4a7ca2f
Berke Kartal <shallwebegin@users.noreply.github.com>
2024-01-01 22:27:46 +0300

    add FutureBuilder

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -16,25 +16,28 @@ class GroceryList extends StatefulWidget {
 
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
-  var _isLoading = true;
+  late Future<List<GroceryItem>> _loadedItems;
   String? _error;
 
   @override
   void initState() {
     super.initState();
-    _loadItems();
+    _loadedItems = _loadItems();
   }
 
-  void _loadItems() async {
+  Future<List<GroceryItem>> _loadItems() async {
     final url = Uri.https(
         'flutter-prep-ff6cd-default-rtdb.firebaseio.com', 'shopping-list.json');
+
     final response = await http.get(url);
-    print(response.statusCode);
+
     if (response.statusCode >= 404) {
-      setState(() {
-        _error = 'Failed to fetch data. Please try again later';
-      });
+      // setState(() {
+      //   _error = 'Failed to fetch data. Please try again later';
+      // });
+      throw Exception('Failed to fetch grocery items. Please try again later.');
     }
+
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
 
@@ -61,10 +64,7 @@ class _GroceryListState extends State<GroceryList> {
       }
     }
 
-    setState(() {
-      _groceryItems = loadedItems;
-      _isLoading = false;
-    });
+    return loadedItems;
   }
 
   void _addItem() async {
@@ -99,40 +99,6 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
-    Widget content = const Center(child: Text('No items added yet.'));
-    if (_isLoading) {
-      content = const Center(
-        child: CircularProgressIndicator(),
-      );
-    }
-
-    if (_groceryItems.isNotEmpty) {
-      content = ListView.builder(
-        itemCount: _groceryItems.length,
-        itemBuilder: (ctx, index) => Dismissible(
-          onDismissed: (direction) {
-            _removeItem(_groceryItems[index]);
-          },
-          key: ValueKey(_groceryItems[index].id),
-          child: ListTile(
-            title: Text(_groceryItems[index].name),
-            leading: Container(
-              width: 24,
-              height: 24,
-              color: _groceryItems[index].category.color,
-            ),
-            trailing: Text(
-              _groceryItems[index].quantity.toString(),
-            ),
-          ),
-        ),
-      );
-    }
-    if (_error != null) {
-      content = Center(
-        child: Text(_error!),
-      );
-    }
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
@@ -143,7 +109,46 @@ class _GroceryListState extends State<GroceryList> {
           ),
         ],
       ),
-      body: content,
+      body: FutureBuilder(
+        future: _loadedItems,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                snapshot.error.toString(),
+              ),
+            );
+          }
+          if (snapshot.data!.isEmpty) {
+            return const Center(child: Text('No items added yet.'));
+          }
+          return ListView.builder(
+            itemCount: snapshot.data!.length,
+            itemBuilder: (ctx, index) => Dismissible(
+              onDismissed: (direction) {
+                _removeItem(snapshot.data![index]);
+              },
+              key: ValueKey(snapshot.data![index].id),
+              child: ListTile(
+                title: Text(snapshot.data![index].name),
+                leading: Container(
+                  width: 24,
+                  height: 24,
+                  color: snapshot.data![index].category.color,
+                ),
+                trailing: Text(
+                  snapshot.data![index].quantity.toString(),
+                ),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
